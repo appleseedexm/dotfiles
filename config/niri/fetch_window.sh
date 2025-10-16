a59032e0c17e302bb36fd3356204df91ede57798
appleseedexm <appleseedexm@gmail.com>
2025-10-16 09:47:09 +0200

    niri improve fetch to work with any types of workspaces cross monitor

--- config/niri/fetch_window.sh
+++ config/niri/fetch_window.sh
@@ -1,15 +1,41 @@
 #!/bin/bash
 
 WINDOWS=$(niri msg -j windows)
-DP1_WORKSPACE=$(niri msg -j workspaces | jq | jq ".[] | select(.is_active == true and .output == \"DP-1\") | .idx")
-DP2_WORKSPACE=$(niri msg -j workspaces | jq | jq ".[] | select(.is_active == true and .output == \"DP-2\") | .idx")
-WORKSPACE_FOCUSED=$(niri msg -j workspaces | jq | jq ".[] | select(.is_focused == true) | .idx")
-WINDOW_TO_FOCUS=$(echo "$WINDOWS" | jq -r 'map("\(.title) - \(.app_id) - \(.id)") | .[]' | tofi | awk -F- '{print $NF}')
+WORKSPACES=$(niri msg -j workspaces)
+DP1_WORKSPACE=$(echo "$WORKSPACES" | jq ".[] | select(.is_active == true and .output == \"DP-1\") | .idx")
+DP2_WORKSPACE=$(echo "$WORKSPACES" | jq ".[] | select(.is_active == true and .output == \"DP-2\") | .idx")
+WORKSPACE_FOCUSED_IDX=$(echo "$WORKSPACES" | jq ".[] | select(.is_focused == true) | .idx")
+WORKSPACE_FOCUSED_DISPLAY=$(echo "$WORKSPACES" | jq -r ".[] | select(.is_focused == true) | .output")
+WINDOW_TO_FETCH=$(echo "$WINDOWS" | jq -r 'map("\(.title) - \(.app_id) - \(.id)") | .[]' | tofi | awk -F- '{print $NF}' | xargs)
 
-if [[ -n "$WINDOW_TO_FOCUS" ]] ; then
-    niri msg action focus-window --id $WINDOW_TO_FOCUS
-    niri msg action move-window-to-workspace $WORKSPACE_FOCUSED
-    niri msg action focus-workspace $DP1_WORKSPACE
-    niri msg action focus-workspace $DP2_WORKSPACE
-    niri msg action focus-workspace $WORKSPACE_FOCUSED
+if [[ -n "$WINDOW_TO_FETCH" ]] ; then
+
+    WINDOW_TO_FETCH_WORKSPACE=$(echo "$WINDOWS" | jq --argjson window_id $WINDOW_TO_FETCH '.[] | select(.id == $window_id) | .workspace_id')
+    WINDOW_TO_FETCH_DISPLAY=$(echo "$WORKSPACES" | jq -r --argjson workspace_id $WINDOW_TO_FETCH_WORKSPACE '.[] | select(.id == $workspace_id) | .output')
+
+    if [[ $WINDOW_TO_FETCH_DISPLAY == $WORKSPACE_FOCUSED_DISPLAY ]] ; then
+
+        niri msg action focus-window --id $WINDOW_TO_FETCH
+        niri msg action move-window-to-workspace $WORKSPACE_FOCUSED_IDX
+
+    else
+
+        niri msg action focus-window --id $WINDOW_TO_FETCH
+
+        [[ "$WINDOW_TO_FETCH_DISPLAY" == "DP-1" ]] && \
+            niri msg action move-window-to-monitor-left && \
+            [[ "$WINDOW_TO_FETCH_WORKSPACE" != "$DP1_WORKSPACE" ]] && \
+            niri msg action focus-monitor "DP-1" && \
+            niri msg action focus-workspace $DP1_WORKSPACE
+
+        [[ "$WINDOW_TO_FETCH_DISPLAY" == "DP-2" ]] && \
+            niri msg action move-window-to-monitor-right && \
+            [[ "$WINDOW_TO_FETCH_WORKSPACE" != "$DP2_WORKSPACE" ]] && \
+            niri msg action focus-monitor "DP-2" && \
+            niri msg action focus-workspace $DP2_WORKSPACE
+
+        niri msg action focus-window --id $WINDOW_TO_FETCH
+
+    fi
+
 fi
