64cecdde447dc515b19100bf4cb1ced0a906e2a8
appleseedexm <appleseedexm@gmail.com>
2026-01-16 19:47:22 +0100

    xdg add pictures and screenshots folders

--- home/.zshenv
+++ home/.zshenv
@@ -6,6 +6,8 @@ export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_STATE_HOME="$HOME/.local/state"
 export XDG_DATA_DIRS="/usr/local/share:/usr/share"
 export XDG_CONFIG_DIRS="/etc/xdg"
+export XDG_SCREENSHOTS_DIR="$HOME/Pictures/screenshots"
+export XDG_PICTURES_DIR="$HOME/Pictures"
 
 export EDITOR=nvim
 export TERMINAL="/usr/bin/foot"
--- validate_xdg.sh
+++ validate_xdg.sh
@@ -8,6 +8,8 @@ if [ -z $XDG_DATA_HOME ]; then VALID=false; fi
 if [ -z $XDG_STATE_HOME ]; then VALID=false; fi
 if [ -z $XDG_DATA_DIRS ]; then VALID=false; fi
 if [ -z $XDG_CONFIG_DIRS ]; then VALID=false; fi
+if [ -z $XDG_SCREENSHOTS_DIR ]; then VALID=false; fi
+if [ -z $XDG_PICTURES_DIR ]; then VALID=false; fi
 
 if [ $VALID == false ]; then
   echo "some XDG values are not set! abort"
