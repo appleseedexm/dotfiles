fd1986a44eee9336a02fe3fe7dee9d57f68fa714
appleseedexm <appleseedexm@gmail.com>
2026-07-06 14:59:40 +0200

    niri fetch window fix with new screen order

--- config/niri/scripts/fetch_window.sh
+++ config/niri/scripts/fetch_window.sh
@@ -24,13 +24,13 @@ if [[ -n "$WINDOW_TO_FETCH" ]] ; then
         niri msg action focus-window --id $WINDOW_TO_FETCH
 
         [[ "$WINDOW_TO_FETCH_DISPLAY" == "DP-1" ]] && \
-            niri msg action move-window-to-monitor-left && \
+            niri msg action move-window-to-monitor-right && \
             [[ "$WINDOW_TO_FETCH_WORKSPACE" != "$DP1_WORKSPACE" ]] && \
             niri msg action focus-monitor "DP-1" && \
             niri msg action focus-workspace $DP1_WORKSPACE
 
         [[ "$WINDOW_TO_FETCH_DISPLAY" == "DP-2" ]] && \
-            niri msg action move-window-to-monitor-right && \
+            niri msg action move-window-to-monitor-left && \
             [[ "$WINDOW_TO_FETCH_WORKSPACE" != "$DP2_WORKSPACE" ]] && \
             niri msg action focus-monitor "DP-2" && \
             niri msg action focus-workspace $DP2_WORKSPACE
