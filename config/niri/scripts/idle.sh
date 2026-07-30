38685c5d366355551aa305c17b471d4ab9c9d9c4
appleseedexm <appleseedexm@pm.me>
2026-07-30 10:59:18 +0200

    niri idle closes proton pass

--- config/niri/scripts/idle.sh
+++ config/niri/scripts/idle.sh
@@ -2,5 +2,6 @@
 /usr/bin/swayidle -w \
     timeout 305 'niri msg action power-off-monitors' \
     timeout 300 'sh $HOME/.scripts/mute.sh -s' \
+    timeout 300 'sh $XDG_CONFIG_HOME/niri/scripts/pass.sh -c' \
     timeout 300 'swaylock -f' \
     before-sleep 'swaylock -f'
