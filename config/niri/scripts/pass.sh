4650161b6607ec3d1cc0bfee20e516c0740a9a57
appleseedexm <appleseedexm@pm.me>
2026-07-30 10:56:06 +0200

    pass refactor script for simple open and close

--- config/niri/scripts/pass.sh
+++ config/niri/scripts/pass.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
 
-if [[ -z $(pgrep -f proton-pass) ]] ; then
-    proton-pass &>/dev/null & disown;
-    sleep 1;
+CLOSE=false
+
+while getopts "c" flag; do
+    case "${flag}" in
+        c) CLOSE=true ;;
+    esac
+done
+
+eval_windows() {
+    WINDOWS=$(niri msg -j windows)
+    WINDOW_ID=$(echo "$WINDOWS" | jq -r ".[] | select(.app_id == \"Proton Pass\") | .id" | xargs)
+}
+
+close_window() {
+    if [[ -n  $WINDOW_ID  ]] ; then
+        niri msg action close-window --id $WINDOW_ID
+    fi
+}
+
+if [ $CLOSE = true ]; then
+
+    eval_windows
+    close_window
+    exit 0
+
 fi
 
-TEMP_WORKSPACE_NAME="temp-target-float"
-WINDOWS=$(niri msg -j windows)
-WORKSPACES=$(niri msg -j workspaces)
-WINDOW_TO_FETCH=$(echo "$WINDOWS" | jq -r ".[] | select(.app_id == \"Proton Pass\") | .id" | xargs)
-WINDOW_FLOATING=$(echo "$WINDOWS" | jq -r ".[] | select(.app_id == \"Proton Pass\") | .is_floating" | xargs)
-WORKSPACE_FOCUSED_NAME=$(echo "$WORKSPACES" | jq -r ".[] | select(.is_focused == true) | .name")
+eval_windows
 
-if [[ $WINDOW_FLOATING == "true"  ]] ; then
+if [[ -z  $WINDOW_ID  ]] ; then
 
-    niri msg action move-window-to-workspace --window-id $WINDOW_TO_FETCH startup
-    niri msg action move-window-to-tiling --id $WINDOW_TO_FETCH
-    
+    proton-pass &>/dev/null
+
 else
 
-    if [[ -z $WORKSPACE_FOCUSED_NAME ]] ; then
-        niri msg action set-workspace-name $TEMP_WORKSPACE_NAME
-    fi
-
-    niri msg action move-window-to-floating --id $WINDOW_TO_FETCH
-    niri msg action move-window-to-workspace --window-id $WINDOW_TO_FETCH "$WORKSPACE_FOCUSED_NAME"
-    niri msg action move-floating-window --id $WINDOW_TO_FETCH -x 0 -y 0
-    niri msg action set-window-width --id $WINDOW_TO_FETCH 400
-    niri msg action set-window-height --id $WINDOW_TO_FETCH 250
-
-    if [[ -z $WORKSPACE_FOCUSED_NAME ]] ; then
-        niri msg action unset-workspace-name 
-    fi
-
-    niri msg action focus-window --id $WINDOW_TO_FETCH
+    close_window
 
 fi
