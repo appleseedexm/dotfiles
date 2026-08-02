bd4751c9b072af7f8ebda2cac9b5d120fb94d55f
appleseedexm <appleseedexm@gmail.com>
2026-08-02 22:59:00 +0200

    cleanup

--- home/.scripts/pass.sh
+++ home/.scripts/pass.sh
@@ -10,24 +10,16 @@ done
 
 TITLE=$(pass-cli item list --vault-name $VAULT --output json | jq -r ".items | .[] | .title" | fuzzel --dmenu)
 
-echo $TITLE
-
 if [ -n "$TITLE" ]; then
 
     HASPASSWORD=$(pass-cli item view "pass://$VAULT/$TITLE/password")
 
     # ENTRY=$(pass-cli item view "pass://$VAULT/$TITLE" --output json | jq -r ".item | .content " )
-    #
     # HASPASSWORD=$(echo $ENTRY | jq -r ".note? | select(. != null) ")
-    #
-    # echo $ENTRY | jq
-    # echo +$HASPASSWORD\\
 
     if [[ -n $HASPASSWORD ]]; then
-
         wl-copy $HASPASSWORD && notify-send "Proton" "Copied!" && sleep 5 && wl-copy --clear &
     else
-
         notify-send "Proton" "No password found!"
     fi
 fi
