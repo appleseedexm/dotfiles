321d150e5dd3d9a2cb272eae3d90ea4be5de05fe
appleseedexm <appleseedexm@pm.me>
2026-08-17 22:53:42 +0200

    idle fix for all outputs

--- config/argen/scripts/idle.sh
+++ config/argen/scripts/idle.sh
@@ -2,5 +2,5 @@
 /usr/bin/swayidle -w \
     timeout 300 'sh $XDG_CONFIG_HOME/argen/scripts/lock.sh' \
     timeout 300 'sh $HOME/.scripts/mute.sh -s' \
-    timeout 305 'wlopm --off *' \
-        resume 'wlopm --on *'
+    timeout 305 'wlopm --off \*' \
+        resume 'wlopm --on \*'
