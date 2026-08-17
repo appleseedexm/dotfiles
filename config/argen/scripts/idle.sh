123b37af73b9b758d2f67d471fa7aa43c2a60c87
appleseedexm <appleseedexm@pm.me>
2026-08-17 16:59:52 +0200

    possible fix for screen not turning on

--- config/argen/scripts/idle.sh
+++ config/argen/scripts/idle.sh
@@ -2,7 +2,5 @@
 /usr/bin/swayidle -w \
     timeout 300 'sh $XDG_CONFIG_HOME/argen/scripts/lock.sh' \
     timeout 300 'sh $HOME/.scripts/mute.sh -s' \
-    timeout 305 'wlopm --off DP-1' \
-        resume 'wlopm --on DP-1' \
-    timeout 305 'wlopm --off DP-2' \
-        resume 'wlopm --on DP-2'
+    timeout 305 'wlopm --off *' \
+        resume 'wlopm --on *'
